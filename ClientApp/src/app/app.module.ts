ea117e6f3616b9a26fddf6713f859158dfc913e8
llgor34 <imanoryk6@gmail.com>
2022-03-01 16:34:12 +0100

    Added working login component

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -5,11 +5,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InputComponent } from './components/input/input.component';
 import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, InputComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- ClientApp/src/app/pages/login/login.component.ts
+++ ClientApp/src/app/pages/login/login.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.initializeForm();
   }
 
+  private initializeForm() {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  get username() {
+    return this.loginForm.controls['username'];
+  }
+
+  get password() {
+    return this.loginForm.controls['password'];
+  }
+
+  get passwordTooShort() {
+    return !!this.loginForm.controls['password'].getError('minlength');
+  }
+
+  get passwordEmpty() {
+    return !!this.loginForm.controls['password'].getError('required');
+  }
+
+  resetForm() {
+    this.loginForm.reset();
+  }
+
+  onSubmit() {
+    console.log(this.loginForm);
+  }
 }
