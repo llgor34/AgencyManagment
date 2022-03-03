4565344cb02542b22eaef2776d3d4d04d9443b00
Igor Manoryk <imanoryk6@gmail.com>
2022-03-03 10:35:09 +0100

    Improved routing, fixed layout component visibility

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -2,17 +2,40 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth-guard.service';
 import { LoggedUserGuard } from './auth/loggedUser-guard.service';
+import { LayoutComponent } from './components/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ManageEmployeesComponent } from './pages/manage-employees/manage-employees.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
 
 const routes: Routes = [
+  // redirect routes
   {
     path: '',
     pathMatch: 'full',
     redirectTo: '/login',
   },
+
+  // routes for logged in user
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+        canActivate: [AuthGuard],
+      },
+      {
+        path: 'manage-employees',
+        component: ManageEmployeesComponent,
+        canActivate: [AuthGuard],
+      },
+    ],
+  },
+
+  // routes for not logged in user
   {
     path: 'login',
     component: LoginComponent,
@@ -27,11 +50,6 @@ const routes: Routes = [
     path: 'reset-password',
     component: ResetPasswordComponent,
     canActivate: [LoggedUserGuard],
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-    canActivate: [AuthGuard],
   },
 ];
 
