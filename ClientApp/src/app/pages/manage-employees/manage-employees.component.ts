8fcb9192c57af98de1b6a14cf413493b2d9af007
Igor Manoryk <imanoryk6@gmail.com>
2022-03-03 11:49:01 +0100

    Styles and html added for manageEmployees component

--- ClientApp/src/app/pages/manage-employees/manage-employees.component.ts
+++ ClientApp/src/app/pages/manage-employees/manage-employees.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-manage-employees',
   templateUrl: './manage-employees.component.html',
-  styleUrls: ['./manage-employees.component.css']
+  styleUrls: ['./manage-employees.component.css'],
 })
 export class ManageEmployeesComponent implements OnInit {
+  employees = [1, 2, 3, 4, 5];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
