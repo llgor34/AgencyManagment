5f7f648d757c2e9a2207eb1360d3384df55c4f07
Igor Manoryk <imanoryk6@gmail.com>
2022-03-07 19:30:10 +0100

    Updated auth service

--- ClientApp/src/app/auth/auth.service.ts
+++ ClientApp/src/app/auth/auth.service.ts
@@ -60,6 +60,6 @@ export class AuthService {
 
   async deleteUser(userUid: string) {
     const deleteUser = httpsCallable(this.functions, 'deleteUser');
-    return await deleteUser({ userUid });
+    await deleteUser({ userUid });
   }
 }
