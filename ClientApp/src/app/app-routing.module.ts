f1a748da63c48397f01128b902173dd6c4e20b3a
llgor34 <imanoryk6@gmail.com>
2022-03-13 13:45:17 +0100

    changed preloading strategy to PreloadAllModules

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth-guard.service';
 import { LoggedUserGuard } from './guards/loggedUser-guard.service';
 import { NewEmployeeGuard } from './guards/new-employee.service';
@@ -60,7 +60,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
