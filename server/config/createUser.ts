625040e85c9c32a93563b8513b9097dd395987ec
llgor34 <imanoryk6@gmail.com>
2022-07-06 18:15:32 +0200

    Outsourced logic for checking if user is admin

--- server/config/createUser.ts
+++ server/config/createUser.ts
@@ -1,8 +1,14 @@
 import { projectFirestore, projectAuth } from './firebase';
 import { UserCredentials } from './models/user-credentials.model';
+import { isAdmin } from './isAdmin';
+import { throwError } from './throwError';
 
 export const createUser = async (data: UserCredentials, userUid: string) => {
   const { email, password } = data;
+
+  if (!isAdmin(userUid)) {
+    return throwError('Not enough permission for this operation!');
+  }
 
   const user = await projectAuth.createUser({
     email,
