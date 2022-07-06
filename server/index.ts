e5deb29a917d75146d9f5d0369ab9a5090acb60c
llgor34 <imanoryk6@gmail.com>
2022-07-06 17:47:57 +0200

    initial backend setup

---
+++ server/index.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
